bbb63be274eb7c5c13f92d2084ebf1ee12b4a65b
ritik0215 <118260556+ritik0215@users.noreply.github.com>
2024-03-04 06:31:31 +0530

    datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat the js file new version
+let name = "John";
+let age = 25;
+let isApproved = true;
+let firstName;
+
+
+//number=>2 to power 53-1
+//string=>sequence of characters
+//boolean=>true or false
+//undefined=>variable is declared but not assigned
+//null=>empty value it is a standalone value
+//bigint=>2n to power 53-1
+//symbol=>unique value
+
+//object=>key value pair
