94509e659befb3b458d69ea3cc18fbe1b7a404a4
ritik0215 <118260556+ritik0215@users.noreply.github.com>
2024-03-04 06:53:55 +0530

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score= "100abc";
+console.log(typeof score);
+let score1= Number(score);
+console.log(typeof score1);
+console.log(score1);
+
+
+//"100"=>100
+//true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+//"20acbbc"=>NaN
+
+ let isloggedIn= "";
+
+ let booleanisloggedIn= Boolean(isloggedIn);
+    console.log(booleanisloggedIn);
+
+
+ //for boolean
+ //0=>false
+//1=>true
+//empty string=>false 
+//non empty string=>true  
