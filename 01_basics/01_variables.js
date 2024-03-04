d06c84d7a3bcea65e9a2da43da0e600ef7536824
ritik0215 <118260556+ritik0215@users.noreply.github.com>
2024-03-04 06:10:28 +0530

    variable in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=123467;
+let accountEmail="vishwajeet"
+var accountPassword="598"
+accountCity="pune"
+let accountState;
+
+// accountId=123467; // Error: Assignment to constant variable.
+accountEmail="utkarsh"
+accountPassword="123"   
+accountCity="mumbai"    
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/* do not use var in js use let instread var has scope issue*/
