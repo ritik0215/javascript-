a1701b78c39b12ecd7aea3787fa93cf9ec0c4bd4
ritik0215 <118260556+ritik0215@users.noreply.github.com>
2024-03-04 07:16:22 +0530

    conversion and behaviour of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,22 @@ console.log(score1);
 //1=>true
 //empty string=>false 
 //non empty string=>true  
+
+
+ let str1="hello";
+ let str2="world";
+ let str3= str1+str2;
+    console.log(str3);
+    console.log(typeof str3);   
+
+
+console.log("5"+5);//55
+console.log(5+3+"5");//85
+console.log(5+5);       //10
+console.log("5"+3+5);//535
+console.log("5"+true);//5true
+console.log("5"+false);//5false
+console.log("5"+null);//5null
+console.log("5"+undefined);//5undefined
+console.log("5"+NaN);//5NaN
+console.log("5"+Infinity);//5Infinity    
