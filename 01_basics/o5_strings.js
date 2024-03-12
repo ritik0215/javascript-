ca47e0a751886bae0b0bd6f17ccb257284b3ac03
ritik0215 <118260556+ritik0215@users.noreply.github.com>
2024-03-12 14:22:28 +0530

    string in js

---
+++ 01_basics/o5_strings.js
@@ -0,0 +1,18 @@
+//  const name = 'John'
+//  const repo=40
+//  console.log(`Hello ${name}, you have ${repo} repos.`);
+
+ const gameName="counter strike"
+//  console.log(gameName[0]);
+   
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.substring(0,6))
+console.log(gameName.split(''))
+console.log(gameName.split(' '))
+console.log(gameName.split('r'))
+
+console.log(gameName.charAt(8))
+console.log(gameName.indexOf('r'))
+
